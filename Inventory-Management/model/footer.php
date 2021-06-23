04729bb982f65a39bd35d3708541faebc7056e37
jasminefrnndz <86345798+jasminefrnndz@users.noreply.github.com>
2021-06-23 16:58:57 +0800

    Add files via upload

---
+++ Inventory-Management/model/footer.php
@@ -0,0 +1,4 @@
+<div class="footer">
+    Follow us on <a href="https://www.facebook.com/ChaguanPh" class ="href"><img src = "../img/fb.png" width ="20px" height = "20px"> /ChaguanPh</a> <a href="https://www.instagram.com/chaguan.ph/" class ="href"><img src = "../img/insta.png" width ="20px" height = "20px"> @Chaguan.ph</a>
+    <br>All rights reserved Cha Guan House of Tea © 2021
+</div>
