47ca59305a1f464bafafb079ff2432a533b95682
jasminefrnndz <minfernandez05@gmail.com>
2021-06-23 17:16:50 +0800

    Update index.php
    just an update

--- Inventory-Management/view/index.php
+++ Inventory-Management/view/index.php
@@ -20,7 +20,6 @@ $currentPage = 'user.php';
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
         <!-- Latest compiled JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <link rel="stylesheet" type="text/css" href="../css/product.css">
         <link rel="stylesheet" type="text/css" href="../css/navigation.css">
     </head>
